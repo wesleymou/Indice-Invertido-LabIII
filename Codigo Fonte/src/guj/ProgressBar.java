359b554a4bcaebd7ad338105187115cab6e727fd
wesleymou <wesley.pra@hotmail.com>
2019-09-06 17:53:58 -0300

    Adiciona uma progress bar

---
+++ Codigo Fonte/src/guj/ProgressBar.java
@@ -0,0 +1,45 @@
+package guj;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+
+public class ProgressBar extends JFrame { 
+
+	private static final long serialVersionUID = -4668062439664829448L;
+	// create a frame 
+    private JFrame f;
+    private JProgressBar b; 
+  
+    public void createProgressBar() { 
+    	
+        f = new JFrame("Progresso da indexação");  
+        JPanel p = new JPanel(); 
+        b = new JProgressBar(); 
+  
+        b.setValue(0); 
+        b.setStringPainted(true); 
+
+        p.setLayout(new GridLayout(1, 1));
+   
+        p.add(b); 
+        f.add(p); 
+   
+        f.setSize(500, 100);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        f.setVisible(true);
+    } 
+    
+    public void fill(int i) {
+    	b.setValue(i);
+    }
+    
+    public void closeProgressBar() {
+//    	f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+		f.setVisible(false);
+		f.dispose();
+	}
+} 
